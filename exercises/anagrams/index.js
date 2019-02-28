b297b5d1cb2debd108ea79b4a3d1e8202c445cf5
Natesuri <natesuri@gmail.com>
2019-02-28 16:45:26 -0500

    Anagram solutions

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,93 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB)
+}
+
+const cleanString = str => {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
+
+/*
+// another solution, but might have computational disadvantage
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB)
+}
+
+const cleanString = str => {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
+*/
+
+
+/*
+// his complete 1st solution (3 iterations)
+function anagrams(stringA, stringB) {
+  charMapA = makeMap(stringA)
+  charMapB = makeMap(stringB)
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false
+  }
+
+  for (let char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+const makeMap = str => {
+  charMap = {}
+  for (let letter of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[letter] = charMap[letter]++ || 1
+    }
+  return charMap
+}
+*/
+
+/*
+// my improved after starting his solution
+// just condenses. Still my logic
+*/
+
+/*
+// ugly, but works. My first solution
+function anagrams(stringA, stringB) {
+  fixedA = stringA.replace(/[^\w]/g, "").toLowerCase()
+  fixedB = stringB.replace(/[^\w]/g, "").toLowerCase()
+
+  charMapA = {}
+  charMapB = {}
+
+  same = true
+
+  for (let letter of fixedA) {
+    charMapA[letter]
+      ? charMapA[letter]++
+      : charMapA[letter] = 1
+  }
+
+  for (let letter of fixedB) {
+    charMapB[letter]
+      ? charMapB[letter]++
+      : charMapB[letter] = 1
+  }
+
+  for (let char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) {
+      same = false
+      return same
+    }
+  }
+
+  return fixedA.length === fixedB.length
+    ? same
+    : false
+}
+*/
 
 module.exports = anagrams;
