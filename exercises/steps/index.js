c265cec1d20bd6047331a984e31a946bef6f125a
Natesuri <natesuri@gmail.com>
2019-02-28 17:20:38 -0500

    Steps solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,61 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return
+  }
+
+  if (n === stair.length) {
+    console.log(stair)
+    return steps(n, row + 1)
+  }
+
+  if (stair.length <= row ) {
+    stair += '#'
+  } else {
+    stair += ' '
+  }
+
+  steps(n, row, stair)
+}
+
+
+/*
+// recursive solution
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return
+  }
+
+  if (n === stair.length) {
+    console.log(stair)
+    return steps(n, row + 1)
+  }
+
+  if (stair.length <= row ) {
+    stair += '#'
+  } else {
+    stair += ' '
+  }
+
+  steps(n, row, stair)
+}
+*/
+
+/*
+// my first one
+function steps(n) {
+
+  step = 1
+  stairs = new Array(n).fill(' ')
+  while (step <= n ) {
+    stairs.unshift('#')
+    stairs.pop()
+    console.log(stairs.join(''))
+    step++
+  }
+}
+*/
 
 module.exports = steps;
