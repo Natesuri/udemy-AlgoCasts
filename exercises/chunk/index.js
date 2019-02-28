11c8324b632f6bb0c6eec45d2cde981515638382
Natesuri <natesuri@gmail.com>
2019-02-28 15:39:47 -0500

    Chunked Solution
    Got it on the first go!

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,59 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    start = 0
+    endChunk = size
+    chunkedArray = []
+
+    for (let i = 0; i < array.length; i += size) {
+      chunkedArray.push(array.slice(start, endChunk))
+      start += size
+      endChunk += size
+    }
+
+    return chunkedArray
+}
+
+/*
+// my initial solution
+function chunk(array, size) {
+
+  start = 0
+  endChunk = size
+  chunkedArray = []
+
+  for (let i = 0; i < array.length; i += size) {
+    chunkedArray.push(array.slice(start, endChunk))
+    start += size
+    endChunk += size
+  }
+
+  return chunkedArray
+}
+*/
+
+/*
+// his first solution
+function chunk(array, size) {
+  chunked = []
+
+  for (let chunk of array) {
+    const last = chunked[chunked.length - 1]
+
+    if (!last || last.length === size) {
+      chunked.push([chunk])
+    } else {
+      last.push(chunk)
+    }
+  }
+
+  return chunked
+}
+*/
+
+/*
+
+*/
 
 module.exports = chunk;
